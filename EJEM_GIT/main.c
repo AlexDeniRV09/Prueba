69f375377a6e652dae988a7aac089b3bf5d57f94
Alex Denilson <U17305413@utp.edu.pe>
2022-12-19 23:20:56 -0500

    Version 1.1

---
+++ EJEM_GIT/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: alex_
+ *
+ * Created on 19 de diciembre de 2022, 23:17
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ EJEM_GIT/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=EJEM_GIT.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/EJEM_GIT.production.hex
