0bf3fc6ed84cfd2655c3ac9e9fdb0a5ea8240f49
Alex Denilson <U17305413@utp.edu.pe>
2022-12-19 23:58:16 -0500

    Version 1.2

--- EJEM_GIT/main.c
+++ EJEM_GIT/main.c
@@ -5,14 +5,22 @@
  * Created on 19 de diciembre de 2022, 23:17
  */
 
+/* Inclusion de archivos */
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+/* Constantes y macros */
+
+/* Variables globales */
+
+/* Prototipo de funcion */
+
+/* Programa principal */
 int main(int argc, char** argv) {
 
     return (EXIT_SUCCESS);
 }
+
+/* Definicion de funciones */
 
