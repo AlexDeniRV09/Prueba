95aae173ad39f427041dc852eb14f94556769fe3
Alex Denilson <U17305413@utp.edu.pe>
2022-12-20 00:24:39 -0500

    Version 1.3

--- EJEM_GIT/main.c
+++ EJEM_GIT/main.c
@@ -9,18 +9,30 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <xc.h>
 
 /* Constantes y macros */
 
 /* Variables globales */
 
 /* Prototipo de funcion */
+void Ports_Init(void);
 
 /* Programa principal */
 int main(int argc, char** argv) {
+    
+    /* Inicializacion de funciones */
+    Ports_Init();
+    
+    /* Bucle */
+    while(1){
+        
+    }
 
     return (EXIT_SUCCESS);
 }
 
 /* Definicion de funciones */
+void Ports_Init(void){
 
+}
